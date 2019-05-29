47b7bc870d804c651a3816d83efbb7c7139f5087
Danielly <daniellygj@gmail.com>
2019-05-28 22:03:00 -0300

    builder e dependencias closes #11

--- testpippo/src/main/java/pippo/testpippo/fatec/App.java
+++ testpippo/src/main/java/pippo/testpippo/fatec/App.java
@@ -1,8 +1,9 @@
 package pippo.testpippo.fatec;
 
+import java.io.IOException;
+
 import java.util.Optional;
 
-import ro.pippo.core.Application;
 import ro.pippo.core.Pippo;
 
 /**
@@ -14,11 +15,10 @@ public class App // port 8338
 	
 	public static Optional<String> port = Optional.ofNullable(System.getenv("PORT"));
 	
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws IOException {
         Pippo pippo = new Pippo(new BasicApplication());
         pippo.start();
 
-//        pippo.start(Integer.parseInt(port.orElse("8083")));
+        pippo.start(Integer.parseInt(port.orElse("8083")));
     }
 }
